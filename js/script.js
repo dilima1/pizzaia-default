9a7cda636622b5d34df94429cbe887bb22cb8e13
Edilson Lima <edilson.lima1@hotmail.com>
2022-07-09 13:40:40 -0300

    Finalizando as funçoes auxiliares e refatorando algumas linhas de codigos.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,62 @@
-pizzaJson.map((item, index) => {
-    //função cloneNode faz uma copia de um elemento HTMLm parametro TRUE faz a copia de todos os elementos do pai
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
-    document.querySelector('.pizza-area').append(pizzaItem)
+//funções auxiliares
 
-    //preencher os dados de cada pizza:
+const seleciona = (elemento) => document.querySelector(elemento);
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento);
 
+const abrirModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0;
+    seleciona('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.opacity = 1;
+    }, 150)
+}
+
+const fecharModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+    seleciona('.pizzaWindowArea').style.display = 'none';
+    }, 500)
+}
+
+const botoesFechar = () => {
+    selecionaTodos('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+        item.addEventListener('click', fecharModal)
+    })
+}
+
+const preencheDadosPizzas = (pizzaItem, item) => {
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+}
+
+const preencheDadosModal = (item) => {
+    seleciona('.pizzaBig img').src = item.img;
+    seleciona('.pizzaInfo h1').innerHTML = item.description;
+    seleciona('.pizzaInfo--desc').innerHTML = item.name;
+    seleciona('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
+}
+
+pizzaJson.map((item, index) => {
+    //função cloneNode faz uma copia de um elemento HTMLm parametro TRUE faz a copia de todos os elementos do pai
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+    seleciona('.pizza-area').append(pizzaItem)
+
+    //preencher os dados de cada pizza:
+    preencheDadosPizzas(pizzaItem, item);
 
     //pizza clicada
-
     pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
         e.preventDefault()
 
         //abrir janela modal
-        document.querySelector('.pizzaWindowArea').style.display = 'flex'
+        abrirModal();
     
         //preenchimento dos dados
-
-        pizzaItem.querySelector('.pizza-item--img img').src = item.img
-        pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
-        pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-        pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
-
+        preencheDadosModal(item);
     })
 
-    document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
-        document.querySelector('.pizzaWindowArea').style.display = 'none'
-    })
-
-    document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
-        document.querySelector('.pizzaWindowArea').style.display = 'none'
-    })
+    //fechar modal tanto para desktop quanto para mobile
+    botoesFechar();
 })
